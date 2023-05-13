db579d333b973104931927a15fa9d7ea0000c1a9
Cristian Dreyer <cristianbr.dreyer@gmail.com>
2023-05-13 18:02:19 -0300

    to udpate

--- src/index.ts
+++ src/index.ts
@@ -15,38 +15,17 @@ server.post('/recipe', async (request, reply) => {
 })
 
 server.get('/test', async (request, reply) => {
-    const recipe = await requestRecipe(r1)
-    console.log(recipe);
-    return recipe;
+    // const recipe = await requestRecipe(r1)
+    // console.log(recipe);
+    // return recipe;
+    return { hello: 'world' }
 })
 
-server.listen({ port: 3001 }, (err, address) => {
+const port = parseInt(process.env.PORT || '3000', 10);
+server.listen({ port }, (err, address) => {
     if (err) {
         console.error(err)
         process.exit(1)
     }
     console.log(`Server listening at ${address}`)
 })
-
-const r1 = {
-    ingridients: ['farinha', 'berinjela', 'tomate', 'queijo', 'leite', 'pao'],
-    serves: 2,
-    time: 60,
-    language: "ptbr"
-} as RecipeProps;
-
-const r2 = {
-    ingridients: ['eggs', 'avocado', 'cheese', 'milk', 'bread'],
-    time: 20,
-    language: "en"
-} as RecipeProps;
-
-const r3 = {
-    ingridients: ['berinjela', 'tomate', 'cebola', 'pimentão', 'alho', 'azeite', 'pimenta', 'orégano', 'queijo'],
-    time: 60,
-    language: "ptbr",    
-} as RecipeProps;
- 
-// requestRecipe(rInfo)
-// .then((res) => console.log(res))
-// .catch((err) => console.log(err));
