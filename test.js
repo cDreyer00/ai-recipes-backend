03e1d3a360d61d61e783d9ed9ff83895ab7420c8
Cristian Dreyer <cristianbr.dreyer@gmail.com>
2023-05-18 08:43:03 -0300

    test script to log the directories

---
+++ test.js
@@ -0,0 +1,10 @@
+// run 'ls' command in powershell and print the output
+import { exec } from 'child_process';
+exec('dir', (err, stdout, stderr) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(stdout);
+    }
+);
